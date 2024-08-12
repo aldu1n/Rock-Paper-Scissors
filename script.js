b3f12463c4477d64175b41a561d251e7be30eab1
aldu1n <cnndo14@gmail.com>
2024-08-12 18:34:47 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let wins = 0;
+let ties = 0;
+let losses = 0;
+
+const rockEl = document.getElementById('rock');
+const paperEl = document.getElementById('paper');
+const scissorsEl = document.getElementById('scissors');

bffbf12d2809e838c83b0ffa06a0264f8b475980
aldu1n <cnndo14@gmail.com>
2024-08-12 20:08:49 -0400

    Added user input capture and choices comparison

--- script.js
+++ script.js
@@ -6,18 +6,35 @@ const winEl = document.getElementById('wins');
 const tieEl = document.getElementById('ties');
 const loseEl = document.getElementById('losses');
 
+const choicesDiv = document.getElementById('el-choices');
 const rockEl = document.getElementById('rock');
 const paperEl = document.getElementById('paper');
 const scissorsEl = document.getElementById('scissors');
 
-const computerPlay = function() {
-    const computerChoises = ['rock', 'paper', 'scissors'];
-    const randomChoice = Math.floor(Math.random() * computerChoises.length)
-    return computerChoises[randomChoice];
-}
+const choices = ['rock', 'paper', 'scissors'];
+const index = Math.floor(Math.random() * choices.length);
+const computerChoise = choices[index];
 
-const startGame = function(e) {
-    console.log(computerPlay())
-}
+choicesDiv.addEventListener('click', function(e) {
+    let userChoice;
+    if (e.target.matches('.choice')) {
+       userChoice = e.target.getAttribute('id');
+       console.log(userChoice, computerChoise)
 
-rockEl.addEventListener('click', startGame());
+       if (userChoice === computerChoise) {
+           ties++;
+           window.alert("It's a tie!");
+         } else if (
+           (userChoice === 'rock' && computerChoise === 'scissors') || 
+           (userChoice === 'paper' && computerChoise === 'rock') || 
+           (userChoice === 'scissors' && computerChoise === 'paper')
+         ) {
+           wins++;
+           window.alert("You win!");
+         } else {
+           losses++;
+           window.alert("You lost!");
+         }
+    };
+
+});

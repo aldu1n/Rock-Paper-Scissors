632c61dc233d1b6b174437fd86dc22b681594c98
aldu1n <cnndo14@gmail.com>
2024-08-12 20:59:04 -0400

    Edits to computer logic, html, and css

--- script.js
+++ script.js
@@ -12,17 +12,24 @@ const paperEl = document.getElementById('paper');
 const scissorsEl = document.getElementById('scissors');
 
 const choices = ['rock', 'paper', 'scissors'];
+
+const compRandom = function(){
 const index = Math.floor(Math.random() * choices.length);
-const computerChoise = choices[index];
+const result = choices[index];
+return result
+}
+
 
 choicesDiv.addEventListener('click', function(e) {
     let userChoice;
     if (e.target.matches('.choice')) {
+       let computerChoise = compRandom();
        userChoice = e.target.getAttribute('id');
        console.log(userChoice, computerChoise)
 
        if (userChoice === computerChoise) {
            ties++;
+           tieEl.textContent = 'Ties: ' + ties;
            window.alert("It's a tie!");
          } else if (
            (userChoice === 'rock' && computerChoise === 'scissors') || 
@@ -30,11 +37,14 @@ choicesDiv.addEventListener('click', function(e) {
            (userChoice === 'scissors' && computerChoise === 'paper')
          ) {
            wins++;
+           winEl.textContent = 'Wins: ' + wins;
            window.alert("You win!");
          } else {
            losses++;
+           loseEl.textContent = 'Losses: ' + losses;
            window.alert("You lost!");
          }
+         computerChoise = '';
     };
 
 });
